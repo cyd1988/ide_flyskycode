c08f52f1747c25f572b8cb8cb13198d523bbe31f
cyd <291000968@qq.com>
2021-07-14 05:59:36 +0000

    优化列表

--- src/autoregister/lists.ts
+++ src/autoregister/lists.ts
@@ -8,8 +8,8 @@ import { apiModel } from './../lib/apiModel';
 
 export async function fun_list(lists: AnyObj, call_list?: (...args: any[]) => any) {
   let arr: any = [];
-  let fig = {
-    'list': '',
+  let list_label: any = [];
+  let fig: any = {
     canPickMany: false,               // 一个可选标志，使选择器接受多个选择，如果为true，则结果为选择数组。
     ignoreFocusOut: true,            // 设置为true可以在焦点移到编辑器的另一部分或另一个窗口时保持选择器处于打开状态。
     matchOnDescription: true,        // 筛选标志时包含描述的可选标志。
@@ -17,9 +17,18 @@ export async function fun_list(lists: AnyObj, call_list?: (...args: any[]) => an
     placeHolder: '选择要打开的文件？',  // 在输入框
   };
 
+  if (lists.hasOwnProperty('list_label')) {
+    list_label = lists['list_label'];
+  }
+
   if (lists.hasOwnProperty('list')) {
-    fig = Util.merge(true, fig, lists);
-    // delete (fig['list']);
+    for (const key in fig) {
+      if (Object.prototype.hasOwnProperty.call(fig, key)) {
+        if (Object.prototype.hasOwnProperty.call(lists, key)) {
+          fig[key] = lists[key];
+        }
+      }
+    }
     lists = lists['list'];
   }
 
@@ -58,6 +67,34 @@ export async function fun_list(lists: AnyObj, call_list?: (...args: any[]) => an
         console.log('value', value);
         return;
       }
+
+
+      if (value.hasOwnProperty('to->list_label')) {
+
+        let la_sp = value['to->list_label'].split(',,');
+        let list_label_son = [];
+
+        if (list_label.length > 0) {
+
+          let label_ar: [] = la_sp[1].split(',');
+
+          for (let index = 0; index < list_label.length; index++) {
+            let element: any = list_label[index];
+
+            // 过滤列表
+            if (label_ar.find((v) => v == element.label)) {
+              if (element.hasOwnProperty('to->v')) {
+                element = Util.str_to_obj(element, element['to->v'], value.value);
+              }
+              list_label_son.push(element);
+            }
+
+          }
+        }
+
+        value = Util.str_to_obj(value, la_sp[0], list_label_son);
+      }
+
 
       if (value.hasOwnProperty('run')) {
         Api.run(value);
